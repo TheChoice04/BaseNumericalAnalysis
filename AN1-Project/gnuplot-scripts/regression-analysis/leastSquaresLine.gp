19efbcd9c25e6f33cb4420f1bcc9ce7460a58e70
eOnofri04 <elia.onofri@mensa.it>
2019-02-13 17:38:57 +0100

    least square for straight lines

--- AN1-Project/gnuplot-scripts/regression-analysis/leastSquaresLine.gp
+++ AN1-Project/gnuplot-scripts/regression-analysis/leastSquaresLine.gp
@@ -5,8 +5,10 @@ set yzeroaxis
 set title "Least Squares Straight Line Approximation"
 
 f(x) = m*x + q
-fit f(x) "results/regression-analysis/dataCloud.txt" using 1:2 via m,q
+fit f(x) "results/regression-analysis/dataCloud.txt" u 1:2 via m,q
 
-plot "results/regression-analysis/leastSquares.txt" w l title "Regression",\
-     "results/regression-analysis/dataCloud.txt" title "Points",\
-     f(x) title "Gnu-Plot approximation"
+plot "results/regression-analysis/leastSquaresLine.txt" u 1:2 w l title "Regression",\
+     "" u 1:3 w p title "Points",\
+     "" u 1:4 w i title "Error"
+     
+#f(x) title "Gnu-Plot approximation"
--- AN1-Project/src/regression-analysis/least-squares.c
+++ AN1-Project/src/regression-analysis/least-squares.c
@@ -49,6 +49,11 @@ int leastSquare(Matrix P, int n, int ord){
  *	This method evaluates the straight line across the `n` points
  *	 such that its distance is as minor as possible.
  *
+ *	It also write the results on the file:
+ *	 `results/regression-analysis/leastSquaresLine`
+ *	in the format:
+ *	 `x y ya err
+ *
  *	@param P Matrix: points matrix `n \times 2`
  *	@param n int: number of points
  *
@@ -59,7 +64,69 @@ int leastSquare(Matrix P, int n, int ord){
  *************************************************************************/
 
 int leastSquareLine(Matrix P, int n){
+	int i;              // counter
+	double x, y;        // points values
+	double ya;          // y approximation
+	double A00 = n;     // A[0,0]
+	double A01 = 0.0;   // A[1,0] = A[0,1]
+	double A11 = 0.0;   // A[1,1]
+	double b0 = 0.0;    // b[0]
+	double b1 = 0.0;    // b[1]
+	double det;         // determinant of the matrix
+	double m;           // angular coefficient of line
+	double q;           // known term of the line
+	double res = 0.0;   // residue
+	double err;         // control point errors
+	double min, max;    // min and max of the representation
+	FILE *fileP;        // output file pointer
 
+	min = P[0][0];
+	max = min;
+
+	for (i = 0; i < n; i++){
+		x = P[i][0];
+		y = P[i][1];
+		A01 += x;
+		A11 += x*x;
+		b0 += y;
+		b1 += x*y;
+
+		if (min > x)
+			min = x;
+		else if (max < x)
+			max = x;
+	}
+
+	det = (A00 * A11) - (A01 * A01);
+	q = ((b0 * A11) - (A01 * b1)) / det;
+	m = ((A00 * b1) - (b0 * A01)) / det;
+
+	fileP = fopen("results/regression-analysis/leastSquaresLine.txt", "w");
+
+	if (fileP == NULL){
+		printf("ERROR: can't open `results/regression-analysis/leastSquaresLine.txt` in writing mode.");
+		exit(1);
+	}
+
+	for (i = 0; i < n; i++){
+		x = P[i][0];
+		y = P[i][1];
+		ya = (m * x) + q;
+
+		err = fabs(ya - y);
+		res += (err * err);
+
+		fprintf(fileP, ""flfpf" "flfpf" "flfpf" %10le\n", x, ya, y, err);
+
+	}
+
+	fclose(fileP);
+
+	res *= 0.5;
+
+	printf("The points could be approximated by the straight line:\n");
+	printf("\ty = "lfpf"x + "lfpf"\n", m, q);
+	printf("And its residue is %10le.\n", res);
 
 
 	return 0;
